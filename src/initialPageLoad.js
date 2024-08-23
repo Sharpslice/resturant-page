e4707aee1a573c9ff8f9065bdda76e12b03c4fa8
sharpslice <davidlein1440p@gmail.com>
2024-08-22 20:51:46 -0700

    added hours

--- src/initialPageLoad.js
+++ src/initialPageLoad.js
@@ -19,5 +19,27 @@ export default function createFrontPage(){
     subText.textContent = "Our meats are so dry but is sure to get your mouth watering";
     subText.classList.add('subtext');
     contentDiv.appendChild(subText);
+
+    const hours = document.createElement("ul");
+    const hoursList ={
+        Monday : '9:00 am - 6:00 pm',
+        Tuesday : 'Closed',
+        Wednesday : '9:00 am - 6:00 pm',
+        Thursday : '9:00 am - 6:00 pm',
+        Friday : '9:00 am - 6:00 pm',
+        Saturday : '9:00 am - 6:00 pm',
+        Sunday : '9:00 am - 6:00 pm',
+    }
+        for(const days in hoursList){
+            console.log(days)
+            const day = document.createElement("li");
+            day.textContent=days +" "+ hoursList[days];
+            hours.appendChild(day);
+        }
+            
+        
+    
+    contentDiv.appendChild(hours)
+
     console.log("createFrontPage");
 }
